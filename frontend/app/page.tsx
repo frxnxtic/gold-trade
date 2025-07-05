0db6d4c3bca41f29050f9abdac4742617eeb2708
Denys Kozlov <denys.kozlov.work@gmail.com>
2025-07-05 16:17:37 +0200

    quick fix #5

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import FlatCard from "@/components/FlatCard";
 import ContactForm from "@/components/ContactForm";
 
 export default async function Home() {
-  const res = await fetch("https://gold-trade-be/api/izba?id=all", { cache: "no-store" });
+  const res = await fetch("https://gold-trade-be.vercel.app/api/izba?id=all", { cache: "no-store" });
   const flats = await res.json();
 
   return (
